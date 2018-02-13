eb906c9078989f67b5dcdd8dfdaf67159f634d3b
Carlo Renoria <itechcarlo@gmail.com>
2018-02-13 20:22:08 +0800

    first commit

--- expresstut.js
+++ expresstut.js
@@ -2,12 +2,219 @@ var express = require('express');
 
 var app = express();
 
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+// MORE IMPORTS HERE
+
+app.use(require('body-parser').urlencoded({extended: true}));
+
+var formidable = require('formidable');
+
+var credentials = require('./credentials.js');
+app.use(require('cookie-parser')(credentials.cookieSecret));
+
 app.set('port', process.env.PORT || 3000);
 
+app.use(express.static(__dirname + '/public'));
+
+
 app.get('/', function(req, res){
-	res.send('Express is working');
+	res.render('evaluation/part0');
 });
 
-app.listen(app.get('port'), function(){
-	console.log('Express started press Ctrl-C to terminate');
+app.get('/part1', function(req, res){
+	res.render('evaluation/part1');
+});
+
+app.get('/part2', function(req, res){
+	res.render('evaluation/part2');
+});
+
+app.get('/part3', function(req, res){
+	res.render('evaluation/part3');
+});
+
+app.get('/part4', function(req, res){
+	res.render('evaluation/part4');
+});
+
+app.get('/part5', function(req, res){
+	res.render('evaluation/part5');
+});
+
+app.get('/part6', function(req, res){
+	res.render('evaluation/part6');
+});
+
+app.get('/part7', function(req, res){
+	res.render('evaluation/part7');
+});
+
+app.get('/part8', function(req, res){
+	res.render('evaluation/part8');
+});
+
+app.get('/part9', function(req, res){
+	res.render('evaluation/part9');
+});
+
+app.get('/part10', function(req, res){
+	res.render('evaluation/part10');
+});
+
+app.get('/part11', function(req, res){
+	res.render('evaluation/part11');
+});
+
+app.get('/part12', function(req, res){
+	res.render('evaluation/part12');
+});
+
+app.get('/part13', function(req, res){
+	res.render('evaluation/part13');
+});
+
+app.get('/part14', function(req, res){
+	res.render('evaluation/part14');
+});
+
+app.use(function(req, res, next){
+	console.log("Looking for URL : " + req.url);
+	next();
+});
+
+app.get('/junk', function(req, res, next){
+	console.log('Tried to access /junk');
+	throw new Error('/junk doesn\'t exist');
+});
+
+app.use(function(err, req, res, next){
+	console.log('Error : ' + err.message);
+	next();
+});
+
+app.get('/about', function(req, res){
+	res.render('about');
+});
+
+app.get('/contact', function(req, res){
+	res.render('contact', {csrf: 'CSRF token here'});
+});
+
+app.get('/thankyou', function(req, res){
+	res.render('thankyou');
+});
+
+app.post('/process', function(req, res){
+	console.log('Form : ' + req.query.form);
+	console.log('CSRF token : ' + req.body._csrf);
+	console.log('Email : ' + req.body.email);
+	console.log('Question : ' + req.body.ques);
+	res.redirect(303, '/thankyou');
 })
+
+app.get('/file-upload', function(req, res){
+	var now = new Date();
+	res.render('file-upload', {
+		year: now.getFullYear(),
+		month: now.getMonth() });
+});
+
+app.post('/file-upload/:year/:month', function(req, res){
+		var form = new formidable.IncomingForm();
+		form.parse(req, function(err, fields, file){
+			if(err)
+				return res.redirect(303, '/error');
+			console.log('Received file');
+
+			console.log(file);
+			res.redirect(303, '/thankyou');
+		});
+	});
+
+app.get('/cookie', function(req, res){
+	res.cookie('username', 'Carlo Renoria', {expire: new Date() + 9999}).send('username has the value of Carlo Renoria');
+});
+
+app.get('/listcookies', function(req, res){
+	console.log("Cookies : ", req.cookies);
+	res.send('Look in the console for cookies');
+});
+
+app.get('/deletecookie', function(req, res){
+	res.clearCookie('username');
+	res.send('username Cookie Deleted');
+});
+
+var session = require('express-session');
+
+var parseurl = require('parseurl');
+
+app.use(session({
+	resave: false,
+	saveUninitialized: true,
+	secret: credentials.cookieSecret,
+}));
+
+app.use(function(req, res, next){
+	var views = req.session.views;
+
+	if(!views){
+		views = req.session.views = {};
+	}
+
+	var pathname = parseurl(req).pathname;
+
+	views[pathname] = (views[pathname] || 0) + 1;
+	next();
+});
+
+app.get('/viewcount', function(req, res, next){
+	res.send('You viewed this page ' + req.session.views['/viewcount'] + ' times');
+});
+
+var fs = require('fs');
+
+app.get('/readfile', function(req, res, next){
+	fs.readFile('./public/ojt.txt',
+		function(err, data){
+			if(err){
+				return console.error(err);
+			}
+			res.send("the File : " + data.toString());
+		});
+});
+
+app.get('/writefile', function(req, res, next){
+	fs.writeFile('./public/randomfile2.text','More Random Text', function(err){
+		if(err){
+			return console.error(err);
+		}
+	});
+
+	fs.readFile('./public/randomfile2.txt', function(err, data){
+		if(err){
+			return console.error(err);
+		}
+		res.send("The File " + data.toString());
+	});
+});
+
+app.use(function(req, res){
+	res.type('text/html');
+	res.status(404);
+	res.render('404');
+});
+
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+})
+
+app.listen(app.get('port'), function(){
+	console.log("Express started on http://localhost:" + app.get('port') + ' press Ctrl-C to terminate');
+});
